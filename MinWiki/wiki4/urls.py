15c2bba24f85d3abdf49a637ca8660bcbff479a8
Nryder1102 <45008433+Nryder1102@users.noreply.github.com>
2019-06-06 13:17:03 -0400

    Add files via upload

--- MinWiki/wiki4/urls.py
+++ MinWiki/wiki4/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='wiki4'),
+    path('', views.wiki4, name='wiki4'),
+    path('blocks', views.blocks4, name='bindex4.html'),
+    path('zones', views.zones4, name='zindex4.html'),
+    path('enemies', views.enemies4, name='eindex4.html'),
+    path('items', views.items4, name='iindex4.html'),
+    path('weapons', views.weapons4, name='windex4.html'),
+    path('mechs', views.mechs4, name='mindex4.html'),
+    
 ]
--- MinWiki/wiki4/views.py
+++ MinWiki/wiki4/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+# Create your views here.
+def wiki4(request):
+    template = loader.get_template('index4.html')
+    return HttpResponse(template.render())
+
+def blocks4(request):
+    template = loader.get_template('bindex4.html')
+    return HttpResponse(template.render())
+
+def zones4(request):
+    template = loader.get_template('zindex4.html')
+    return HttpResponse(template.render())
+
+def enemies4(request):
+    template = loader.get_template('eindex4.html')
+    return HttpResponse(template.render())
+
+def items4(request):
+    template = loader.get_template('iindex4.html')
+    return HttpResponse(template.render())
+
+def weapons4(request):
+    template = loader.get_template('windex4.html')
+    return HttpResponse(template.render())
+
+def mechs4(request):
+    template = loader.get_template('mindex4.html')
+    return HttpResponse(template.render())
