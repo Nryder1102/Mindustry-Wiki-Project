66730873412d6096daa8c1b5d0d584591f4472cb
Nryder1102 <45008433+Nryder1102@users.noreply.github.com>
2019-06-07 13:47:44 -0400

    Add files via upload

--- MinWiki/wiki4/urls.py
+++ MinWiki/wiki4/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+    #Index paths
     path('', views.wiki4, name='wiki4'),
     path('blocks', views.blocks4, name='bindex4.html'),
     path('zones', views.zones4, name='zindex4.html'),
@@ -10,5 +12,24 @@ urlpatterns = [
     path('items', views.items4, name='iindex4.html'),
     path('weapons', views.weapons4, name='windex4.html'),
     path('mechs', views.mechs4, name='mindex4.html'),
-    
+
+    #Item paths
+    path('items/copper', views.copper, name='copper.html'),
+    path('items/coal', views.coal, name='coal.html'),
+    path('items/blast compound', views.bcompound, name='blast compound.html'),
+    path('items/graphite', views.graphite, name='graphite.html'),
+    path('items/lead', views.lead, name='lead.html'),
+    path('items/metaglass', views.mglass, name='metaglass.html'),
+    path('items/phase fabric', views.pfab, name='phase fabric.html'),
+    path('items/plastanium', views.plastanium, name='plastanium.html'),
+    path('items/pyratite', views.pyratite, name='pyratite.html'),
+    path('items/sand', views.sand, name='sand.html'),
+    path('items/scrap', views.scrap, name='scrap.html'),
+    path('items/silicon', views.silicon, name='silicon.html'),
+    path('items/spore pod', views.spod, name='spore pod.html'),
+    path('items/surge alloy', views.salloy, name='surge alloy.html'),
+    path('items/thorium', views.thorium, name='thorium.html'),
+    path('items/titanium', views.titanium, name='titanium.html'),
+
+
 ]
--- MinWiki/wiki4/views.py
+++ MinWiki/wiki4/views.py
@@ -31,3 +31,72 @@ def weapons4(request):
 def mechs4(request):
     template = loader.get_template('mindex4.html')
     return HttpResponse(template.render())
+
+#Items things
+
+def copper(request):
+    template = loader.get_template('copper.html')
+    return HttpResponse(template.render())
+
+def coal(request):
+    template = loader.get_template('coal.html')
+    return HttpResponse(template.render())
+
+def bcompound(request):
+    template = loader.get_template('blast compound.html')
+    return HttpResponse(template.render())
+
+def graphite(request):
+    template = loader.get_template('graphite.html')
+    return HttpResponse(template.render())
+
+def lead(request):
+    template = loader.get_template('lead.html')
+    return HttpResponse(template.render())
+
+def mglass(request):
+    template = loader.get_template('metaglass.html')
+    return HttpResponse(template.render())
+
+def pfab(request):
+    template = loader.get_template('phase fabric.html')
+    return HttpResponse(template.render())
+
+def plastanium(request):
+    template = loader.get_template('plastanium.html')
+    return HttpResponse(template.render())
+
+def pyratite(request):
+    template = loader.get_template('pyratite.html')
+    return HttpResponse(template.render())
+
+def sand(request):
+    template = loader.get_template('sand.html')
+    return HttpResponse(template.render())
+
+def scrap(request):
+    template = loader.get_template('scrap.html')
+    return HttpResponse(template.render())
+
+def silicon(request):
+    template = loader.get_template('silicon.html')
+    return HttpResponse(template.render())
+
+def spod(request):
+    template = loader.get_template('spore pod.html')
+    return HttpResponse(template.render())
+
+def salloy(request):
+    template = loader.get_template('surge alloy.html')
+    return HttpResponse(template.render())
+
+def thorium(request):
+    template = loader.get_template('thorium.html')
+    return HttpResponse(template.render())
+
+def titanium(request):
+    template = loader.get_template('titanium.html')
+    return HttpResponse(template.render())
+
+
+#
